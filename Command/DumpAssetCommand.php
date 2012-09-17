83a663e11ffc8a7e9556bb06c6d0692e07378c3c
Benjamin Seibert <benjamin.seibert@gmx.de>
2012-09-17 17:27:06 +0200

    fixed css rewrite

--- Command/DumpAssetCommand.php
+++ Command/DumpAssetCommand.php
@@ -100,25 +100,58 @@ class DumpAssetCommand extends ContainerAwareCommand
         // rewrite url(..) definitions
         $content = preg_replace_callback('/url\((["\']?)(?P<url>.*?)(\\1)\)/', function($matches) use($baseUrl)
         {
-            $url = $baseUrl.'/'.$matches['url'];
+            $url = DumpAssetCommand::normalizeUrl($matches['url'],$baseUrl);
             return 'url('.$url.')';
         }, $content);
 
         // rewrite @import ... definitions
         $content = preg_replace_callback('/@import (?!url\()(\'|"|)(?P<url>[^\'"\)\n\r]*)\1;?/',function($matches) use($baseUrl)
         {
-            $url = $baseUrl.'/'.$matches['url'];
+            $url = DumpAssetCommand::normalizeUrl($matches['url'],$baseUrl);
             return '@import "'.$url.'"';
         },$content);
 
         // rewrite ie filters like alpha
         $content = preg_replace_callback('/src=(["\']?)(?<url>.*?)\\1/', function($matches) use ($baseUrl)
         {
-            $url = $baseUrl.'/'.$matches['url'];
+            $url = DumpAssetCommand::normalizeUrl($matches['url'],$baseUrl);
             return $url;
         },$content);
 
         return $content;
+    }
+
+    public static function normalizeUrl($url,$baseUrl)
+    {
+
+        if(preg_match('%^((https?://)|(www\.))([a-z0-9-].?)+(:[0-9]+)?(/.*)?$%i',$url))
+        {
+            // absolute url
+            return $url;
+        }
+        elseif(strpos($url,'/')===0)
+        {
+            // absolute host relative url
+            return $baseUrl.$url;
+        }
+        elseif(strpos($url,'./')===0)
+        {
+            // relative url
+            return $baseUrl.substr($url,2);
+        }
+        else
+        {
+            // normalize relative path walks by removing on part from the base url
+            // for every ../ in the target url
+            $parts = explode('/',$url);
+            $baseParts = explode('/',$baseUrl);
+            while($parts[0]==='..')
+            {
+                array_shift($parts);
+                array_pop($baseParts);
+            }
+            return implode('/',$baseParts).'/'.implode('/',$parts);
+        }
     }
 
 
